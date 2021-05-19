9ef7fd3549cee2fc6e974d29d892251d056e108d
abhiv-97 <v.abhishek@outlook.com>
2021-05-19 14:35:34 +0100

    prob1

---
+++ arithmetic.py
@@ -0,0 +1,6 @@
+def arithmetic_arranger(problems, ans=False):
+
+    return False
+
+print(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]))
+print("    3      3801      45      123\n+ 855    -    2    + 43    +  49\n-----    ------    ----    -----")
